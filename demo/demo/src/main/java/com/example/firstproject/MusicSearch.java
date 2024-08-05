1f4af939236a3736e36c85e355d5386013fe90a4
Jeong Seong Bin <tjdqls3124@gmail.com>
2024-08-05 17:05:36 +0900

    Update MusicSearch.java

--- demo/demo/src/main/java/com/example/firstproject/MusicSearch.java
+++ demo/demo/src/main/java/com/example/firstproject/MusicSearch.java
@@ -1,26 +1,62 @@
 package com.example.firstproject;
 
-import org.springframework.http.*;
-import org.springframework.web.client.RestTemplate;
+import com.neovisionaries.i18n.CountryCode;
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.specification.Album;
+import com.wrapper.spotify.requests.data.albums.GetAlbumRequest;
+import org.apache.hc.core5.http.ParseException;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public class MusicSearch {
-    public String search(String accessToken, String q){ //q는 검색어
 
-        RestTemplate rest = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + accessToken);;
-        headers.add("Host", "api.spotify.com");
-        headers.add("Content-type", "application/json");
-        String body = "";
+    private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
+            .setAccessToken(" ")
+            .build();
 
-        HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
-        ResponseEntity<String> responseEntity = rest.exchange("https://api.spotify.com/v1/search?type=track&q=" + q, HttpMethod.GET, requestEntity, String.class);
-        HttpStatus httpStatus = (HttpStatus) responseEntity.getStatusCode();
-        int status = httpStatus.value(); //상태 코드가 들어갈 status 변수
-        String response = responseEntity.getBody();
-        System.out.println("Response status: " + status);
-        System.out.println(response);
+    private static final GetAlbumRequest getAlbumRequest = spotifyApi.getAlbum(" ")
+            .market(CountryCode.KR)
+            .build();
 
-        return response;
+    public static void getAlbum_Sync() {
+        try {
+            // Execute the request synchronous
+            final Album album = getAlbumRequest.execute();
+
+            // Print album's name
+            System.out.println("Name: " + album.getName());
+        } catch (IOException | SpotifyWebApiException e) {
+            System.out.println("Something went wrong!\n" + e.getMessage());
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void getAlbum_Async() {
+        try {
+            // Execute the request asynchronous
+            final Future<Album> albumFuture = getAlbumRequest.executeAsync();
+
+            // Do other things...
+
+            // Wait for the request to complete
+            final Album album = albumFuture.get();
+
+            // Print album's name
+            System.out.println("Name: " + album.getName());
+        } catch (InterruptedException | ExecutionException e) {
+            System.out.println("Something went wrong!\n" + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        // Synchronous call
+        getAlbum_Sync();
+
+        // Asynchronous call
+        getAlbum_Async();
     }
 }
